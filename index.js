38e01da3ab9180fdfed6f542c321ebc38dab0d0c
lymhyt <hayatiyt62@gmail.com>
2023-04-10 15:36:04 +0800

    today done

--- index.js
+++ index.js
@@ -1,118 +1,120 @@
+// import express
 const express = require('express');
+// import JWT tokens
+const jwt = require('jsonwebtoken');
+// start listening on port 3000
 const app = express();
-const port = 3000;
+const port = 4000;
 
 let dbUsers = [
   {
-      username :"yat",
-      password:"pass",
-      name: "yat",
-      email:"email"
-  },
-  //setiap user kene ade lain username
-  {
-      username :"aya",
-      password:"passw",
-      name: "aya",
-      email:"email"
-  }
-]
-//enable json body parsing
-app.use (express.json());
+    username :"yat",
+    password:"pass",
+    name: "yat",
+    email:"email"
+},
+//setiap user kene ade lain username
+{
+    username :"aya",
+    password:"passw",
+    name: "aya",
+    email:"email"
+}
+  ]
 
-app.post('/login',(req,res)=>{
-  let data =req.body
-  res.send(
-    login (
-      data.username,
-      data.password
-    )
-  )
-})
+  //enable json parsing
+app.use(express.json());
 
-app.post('/register',(req,res)=>{
-  let data =req.body
-  res.send(
-    register(
-      data.username,
-      data.password,
-      data.name,
-      data.email
-    )
-  )
-})
+app.get('/', verifyToken,(req, res) => {
+  //console.log(req.user)
+    res.send('Hello World!');
+});
 
-app.listen(port,()=>{
-  console.log('Example app lstening at http://localhost:${port}')
-})
+app.post('/', (req, res) => {
+    res.send(req.body);
+});
 
-function login(username,password){
-  console.log ("someone try to login with", username, password)
-  let matched =dbUsers.find(element =>{
-      element.username==username
-  })
-      //console.log(matched)
-      if (matched)
-      {
+app.post('/login', (req, res) => {
+    let data = req.body
+    
+    const user=login(data.username, data.password);
+    /*res.send(
+        login(
+            data.username,
+            data.password
+        )
+    );*/
+    res.send(generatetoken(user))
+  });
+
+app.post('/register', (req, res) => {
+    let data = req.body
+    res.send(
+        register(
+            data.username,
+            data.password,
+            data.name,
+            data.email
+        )
+    );
+});
+
+app.listen(port, () => {
+    console.log('example app listening on port ${port}')
+});
+
+function login(username, password){
+      console.log("Someone try to login with ", username, password) //apa yg user akan masukkan
+      let matched = dbUsers.find(element => 
+          element.username == username  
+      )// find element
+      if(matched) {
           if (matched.password == password){
               return matched
-          }else{
-              return "Password Not matched"
+          } else {
+              return "Password not matched"
           }
+      } else {
+          return "Username not found"
       }
-      else {
-          return "username not found"
-      }  
   }
 
-function register (
-  newusername, 
-  newpassword, 
-  newname, 
-  newemail
-  ){
 
-    dbUsers.find(element=>{
-      console.log(element)
-    })
-    //to do :check if username exist
-
+function register(newusername, newpassword, newname, newemail){
+    dbUsers.find(element => {
+        console.log(element) 
+      })//check if username exists
     dbUsers.push({
-        username: newusername,
-        password: newpassword,
-        name: newname,
-        email:newemail,
-    })
+          username : newusername,
+          password : newpassword,
+          name : newname,
+          email : newemail
+      })
+      return "New acc has been created"
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//to generate JWT tokens
+function generatetoken(userprofile){
+  return jwt.sign({
+    //data: 'foobar',
+    userprofile,
+  }, 'secret', 
+  { expiresIn: 60 * 60 });
+}
 
-app.post('/',(req,res)=>{
-  let data=req.body
-  res.send('post request'+JSON.stringidy(data))
-})
+function verifyToken(req,res,next){
+  // verify a token symmetric
+  let header =req.header.authorization
+  console.log(header)
 
+  let token=header.split(' ')[1]
+  jwt.verify(token, 'secret', function(err, decoded) {
+    //if(err){
+     // res.send("Invalid Token")
+    //}
 
-//create a POST route for user to login
-//app.post('/login',(req,res)=>{
-  //get username &passw from req body
-  //const {username,password}= req.body;
-  //find user n database
-  //const user=dbUsers.find(user=> user.username ===username && user.password=== password);
-
-  //if (user){
-    //user found then return the user object
-   // res.send(user);
-  //}else{
-    //if user not found display
-    //res.send({error:"USER NOT FOUND"})
-  //}
-//}
-//)
-
-
-//app.listen(port, () => {
-  //console.log(`Example app listening on port ${port}`)
-//})
+    req.user=decoded
+  //console.log(decoded.foo) // bar
+  next()
+});
+}
